729c53e00e9ca57fcacb0ba356aeb85e66524b64
Bibhu Prasad Sahoo <19btcse09@suiit.ac.in>
2023-03-04 12:52:59 +0530

    bug fixed of image urls not working

--- models/product.js
+++ models/product.js
@@ -11,7 +11,7 @@ const productSchema = mongoose.Schema({
         required: true,
         trim: true
     },
-    images: {
+    imageUrls: {
         type: [
             {
                 type: String,
--- routers/admin.js
+++ routers/admin.js
@@ -6,11 +6,27 @@ const Product = require('../models/product');
 // Add a product -->
 adminRouter.post('/admin/add-product', admin, async (request, response) => {
     try {
-        const { name, description, images, quantity, price, category } = request.body;
+        const { name, description, imageUrls, quantity, price, category } = request.body;
         let product = new Product({
-            name, description, images, quantity, price, category
+            name, description, imageUrls, quantity, price, category
         });
         product = await product.save();
+        response.json(product);
+    } catch (e) {
+        response.status(500).json({ error: e.message });
+    }
+});
+
+// Get all the products -->
+adminRouter.get('/admin/get-products', admin, async (request, response) => {
+    try {
+        const { name } = request.body;
+        const product = await Product.find({});
+        if (!product) {
+            return response.status(400).json({
+                message: 'Requested product doesn\t exist!',
+            });
+        }
         response.json(product);
     } catch (e) {
         response.status(500).json({ error: e.message });
